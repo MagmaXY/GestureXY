1c9274fb5059390148e3070c6ae58caf5f5540d8
MagmaXY <149756117+MagmaXY@users.noreply.github.com>
2025-04-01 19:01:28 +0700

    Update gestTick.ino

--- firmware/gestTick.ino
+++ firmware/gestTick.ino
@@ -2,7 +2,7 @@ void gestTick() {
 
   if (g.gest == "") return;
 
-  if (!db[kk::state] and g.gest == db[kk::stateGest]) {
+  if (!db[kk::state] and g.gest == g.getGest(db[kk::stateGest])) {
 
     db[kk::state] = true;
     Serial.println("Active");
@@ -11,7 +11,7 @@ void gestTick() {
     gData.timer = millis();
     return;
 
-  } else if (db[kk::state] and gData.name == 0 and gData.scene == 0 and g.gest == db[kk::stateGest]) {
+  } else if (db[kk::state] and gData.name == 0 and gData.scene == 0 and g.gest == g.getGest(db[kk::stateGest])) {
     db[kk::state] = false;
     Serial.println("Inactive");
     oled.clear();
@@ -23,9 +23,9 @@ void gestTick() {
 
   if (gData.scene == 0) {
 
-    if (g.gest == db[kk::nextNameGest] or g.gest == db[kk::previousNameGest]) {
+    if (g.gest == g.getGest(db[kk::nextNameGest]) or g.gest == g.getGest(db[kk::previousNameGest])) {
 
-      int8_t move = g.gest == db[kk::nextNameGest] ? 1 : -1;
+      int8_t move = g.gest == g.getGest(db[kk::nextNameGest]) ? 1 : -1;
       if (gData.name + move < 0) {
         gData.name = getNamesCount() - 1;
       } else {
@@ -35,9 +35,9 @@ void gestTick() {
 
     } else if (gData.name == 0) {
 
-      if (g.gest == db[kk::nextSceneGest] or g.gest == db[kk::previousSceneGest]) {
+      if (g.gest == g.getGest(db[kk::nextSceneGest]) or g.gest == g.getGest(db[kk::previousSceneGest])) {
 
-        int8_t move = g.gest == db[kk::nextSceneGest] ? 1 : -1;
+        int8_t move = g.gest == g.getGest(db[kk::nextSceneGest]) ? 1 : -1;
         if (gData.scene + move < 0) {
           gData.scene = getScenesCount() - 1;
         } else {
@@ -46,20 +46,20 @@ void gestTick() {
         Serial.println(getScene());
         cursorGraph();
 
-      } else if (g.gest == db[kk::plusBrightnessGest]) {
+      } else if (g.gest == g.getGest(db[kk::plusBrightnessGest])) {
 
         db[kk::brightness] = ((byte)db[kk::brightness] + (byte)db[kk::plusBrightness]) % 256;
         cursorGraph();
         oled.setContrast(db[kk::brightness]);
 
-      } else if (g.gest == db[kk::minusBrightnessGest]) {
+      } else if (g.gest == g.getGest(db[kk::minusBrightnessGest])) {
 
         db[kk::brightness] -= (byte)db[kk::plusBrightness];
         if (db[kk::brightness] < 0) db[kk::brightness] = 255;
         cursorGraph();
         oled.setContrast(db[kk::brightness]);
 
-      } else if (g.gest == db[kk::vibrationGest]) {
+      } else if (g.gest == g.getGest(db[kk::vibrationGest])) {
 
         db[kk::vibration] = !db[kk::vibration];
         cursorGraph();
@@ -70,7 +70,7 @@ void gestTick() {
       oled.print(g.gest);
       sendGest();
     }
-  } else if (g.gest == db[kk::nextSceneGest] or g.gest == db[kk::previousSceneGest]) {
+  } else if (g.gest == g.getGest(db[kk::nextSceneGest]) or g.gest == g.getGest(db[kk::previousSceneGest])) {
 
     int8_t move = g.gest == db[kk::nextSceneGest] ? 1 : -1;
     if (gData.scene + move < 0) {
@@ -81,7 +81,7 @@ void gestTick() {
     Serial.println(getScene());
     cursorGraph();
 
-  } else if (g.gest == db[kk::sceneGest]) {
+  } else if (g.gest == g.getGest(db[kk::sceneGest])) {
 
     Serial.println(getScene());
     cursorGraph();
