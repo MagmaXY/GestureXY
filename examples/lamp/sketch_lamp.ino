2693e797ac62711a817e5ea5977cb872a1cbff94
MagmaXY <149756117+MagmaXY@users.noreply.github.com>
2025-04-02 19:40:12 +0700

    Create sketch_lamp.ino

---
+++ examples/lamp/sketch_lamp.ino
@@ -0,0 +1,126 @@
+//#include <ESP8266WiFi.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "SettingsGyver.h"
+#include "GyverDBFile.h"
+#include "LittleFS.h"
+#include <FastLED.h>
+
+#include "GestureXY.h"
+
+WiFiClient espClient;
+PubSubClient mqtt(espClient);
+GestureXY g;
+
+DB_KEYS(
+  kk,
+  ssid,
+  pass,
+  ssid1,
+  pass1,
+  host,
+  port,
+  local,
+  remote,
+  header,
+  serial,
+  brightness,
+  state,
+  stateGest,
+  tempGest,
+  plusBrightnessGest,
+  plusBrightness,
+  minusBrightnessGest,
+  nextGest,
+  previousGest);
+;
+
+GyverDBFile db(&LittleFS, "🔦 LampXY.db");
+SettingsGyver sett((String) "🏮 LampXY", &db);
+
+struct gestureData {
+  byte scene = 0;
+  String scenes[4] = { "Нет", "Холодный", "Нормальный", "Тёплый" };
+  byte colors[8][3]{
+    { 255, 0, 0 },
+    { 255, 110, 0 },
+    { 255, 255, 0 },
+    { 0, 255, 0 },
+    { 0, 255, 110 },
+    { 0, 0, 255 },
+    { 255, 0, 110 },
+    { 255, 0, 255 }
+  };
+  byte color = 6;
+} gData;
+
+#define PIN 13
+#define NUM 64
+#define TYPE WS2812
+#define ORDER GRB
+
+CRGB leds[NUM];
+
+void setup() {
+  db_init();
+  Serial.begin(db[kk::serial]);
+  Serial.setTimeout(50);
+  FastLED.addLeds<TYPE, PIN, ORDER>(leds, NUM);
+  FastLED.setBrightness(db[kk::brightness]);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 800);
+
+  for (byte i = 0; i < NUM; i++) {
+    leds[i].setRGB(gData.colors[gData.color][0], gData.colors[gData.color][1], gData.colors[gData.color][2]);
+  }
+  FastLED.show();
+  bool flag = false;
+  byte timer = 0;
+  WiFi.begin(db[kk::ssid], db[kk::pass]);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+    if (!flag) timer++;
+    if (timer > 15 and !flag) {
+      flag = true;
+      WiFi.disconnect();
+      WiFi.begin(db[kk::ssid1], db[kk::pass1]);
+    }
+  }
+  Serial.println("Подключился");
+  for (int i = 0; i < NUM; i++) leds[i].setRGB(0, 0, 0);
+  FastLED.show();
+  sett.begin();
+  sett.config.theme = sets::Colors::Green;
+  sett.onBuild(build);
+  mqtt.setServer(db[kk::host].c_str(), db[kk::port]);
+  mqtt.setCallback(callback);
+  mqtt.setSocketTimeout(1000);
+  connectMQTT();
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  mqttTick();
+  sett.tick();
+}
+
+void light() {
+  if (gData.scenes[gData.scene] == "Нет") {
+    for (int i = 0; i < NUM; i++) {
+      leds[i].setRGB(gData.colors[gData.color][0], gData.colors[gData.color][1], gData.colors[gData.color][2]);
+    }
+  } else if (gData.scenes[gData.scene] == "Холодный") {
+    for (int i = 0; i < NUM; i++) {
+      leds[i].setRGB(0, 213, 255);
+    }
+  } else if (gData.scenes[gData.scene] == "Нормальный") {
+    for (int i = 0; i < NUM; i++) {
+      leds[i].setRGB(255, 255, 255);
+    }
+  } else if (gData.scenes[gData.scene] == "Тёплый") {
+    for (int i = 0; i < NUM; i++) {
+      leds[i].setRGB(255, 200, 0);
+    }
+  }
+  FastLED.show();
+}
