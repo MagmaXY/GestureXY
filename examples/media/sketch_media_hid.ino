3cb7131194dd29eed4a5be0db33f277d27ccf082
MagmaXY <149756117+MagmaXY@users.noreply.github.com>
2025-04-05 18:21:47 +0700

    Create sketch_media_hid.ino

---
+++ examples/media/sketch_media_hid.ino
@@ -0,0 +1,79 @@
+#define GT_PRINT_MODE
+#define GT_MICRO_TX
+
+#include <GyverTransfer.h>
+GyverTransfer<13, GT_TX, 5000> tx;
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "SettingsGyver.h"
+#include "GyverDBFile.h"
+#include "LittleFS.h"
+#include "GestureXY.h"
+#include "ParsingXY.h"
+
+WiFiClient espClient;
+PubSubClient mqtt(espClient);
+GestureXY g;
+
+DB_KEYS(
+  kk,
+  ssid,
+  pass,
+  ssid1,
+  pass1,
+  host,
+  port,
+  local,
+  remote,
+  header,
+  serial,
+  pauseGest,
+  muteGest,
+  backspaceGest,
+  nextGest,
+  previousGest,
+  upGest,
+  downGest,
+  powerGest,
+  sleepGest,
+  wakeGest,
+  pauseScenes,
+  sleepScenes,
+  powerScenes,
+  wakeScenes,
+  sep);
+;
+
+GyverDBFile db(&LittleFS, "🎶 MediaXY.db");
+SettingsGyver sett((String) "🎵 MediaXY", &db);
+
+void setup() {
+
+  Serial.begin(db[kk::serial]);
+  Serial.setTimeout(50);
+  WiFi.begin(db[kk::ssid], db[kk::pass]);
+
+  bool flag = false;
+  byte timer = 0;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+    if (!flag) timer++;
+    if (timer > 15 and !flag) {
+      flag = true;
+      WiFi.disconnect();
+      WiFi.begin(db[kk::ssid1], db[kk::pass1]);
+    }
+  }
+  Serial.println("Подключился");
+  mqtt.setServer(db[kk::host].c_str(), db[kk::port]);
+  mqtt.setCallback(callback);
+  connectMQTT();
+  tx.print("https://github.com/MagmaXY/GestureXY/");
+}
+
+void loop() {
+  mqttTick();
+}
