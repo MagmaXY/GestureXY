cca686151f44363a54baa38d5110f76f37a5e9ab
MagmaXY <149756117+MagmaXY@users.noreply.github.com>
2025-04-17 21:23:02 +0700

    Update sketch_lamp.ino

--- examples/lamp/sketch_lamp.ino
+++ examples/lamp/sketch_lamp.ino
@@ -5,6 +5,7 @@
 #include "GyverDBFile.h"
 #include "LittleFS.h"
 #include <FastLED.h>
+#include <EncButton.h>
 
 #include "GestureXY.h"
 #include "ParsingXY.h"
@@ -12,6 +13,7 @@
 WiFiClient espClient;
 PubSubClient mqtt(espClient);
 GestureXY g;
+Button btn(4);
 
 DB_KEYS(
   kk,
@@ -64,15 +66,17 @@ struct gestureData {
 #define NUM 64
 #define TYPE WS2812
 #define ORDER GRB
+#define GND 15
 
 CRGB leds[NUM];
 
 void setup() {
   db_init();
+  pinMode(GND, OUTPUT);
+  digitalWrite(GND, LOW);
   Serial.begin(db[kk::serial]);
   Serial.setTimeout(50);
   FastLED.addLeds<TYPE, PIN, ORDER>(leds, NUM);
-  FastLED.setBrightness(db[kk::brightness]);
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 800);
 
   for (byte i = 0; i < NUM; i++) {
@@ -83,16 +87,23 @@ void setup() {
   byte timer = 0;
   WiFi.begin(db[kk::ssid], db[kk::pass]);
   while (WiFi.status() != WL_CONNECTED) {
+    if (btn.read()) timer = 15;
     delay(1000);
     Serial.print(".");
-    if (!flag) timer++;
-    if (timer > 15 and !flag) {
-      flag = true;
-      WiFi.disconnect();
-      WiFi.begin(db[kk::ssid1], db[kk::pass1]);
+    timer++;
+    if (timer > 15) {
+      timer = 0;
+      if (!flag) {
+        WiFi.disconnect();
+        WiFi.begin(db[kk::ssid1], db[kk::pass1]);
+        flag = true;
+      } else {
+        ESP.restart();
+      }
     }
   }
   Serial.println("Подключился");
+  FastLED.setBrightness(db[kk::brightness]);
   for (int i = 0; i < NUM; i++) leds[i].setRGB(0, 0, 0);
   FastLED.show();
   sett.begin();
@@ -108,24 +119,42 @@ void setup() {
 void loop() {
   mqttTick();
   sett.tick();
+  btnTick();
 }
 
 void light() {
+  FastLED.setBrightness(db[kk::brightness]);
   for (int i = 0; i < NUM; i++) {
-    switch (gData.scene) {
-      case 0:
-        leds[i].setRGB(gData.colors[gData.color][0], gData.colors[gData.color][1], gData.colors[gData.color][2]);
-        break;
-      case 1:
-        leds[i].setRGB(0, 213, 255);
-        break;
-      case 2:
-        leds[i].setRGB(255, 255, 255);
-        break;
-      case 3:
-        leds[i].setRGB(255, 200, 0);
-        break;
+    if (db[kk::state]) {
+      switch (gData.scene) {
+        case 0:
+          leds[i].setRGB(gData.colors[gData.color][0], gData.colors[gData.color][1], gData.colors[gData.color][2]);
+          break;
+        case 1:
+          leds[i].setRGB(0, 213, 255);
+          break;
+        case 2:
+          leds[i].setRGB(255, 255, 255);
+          break;
+        case 3:
+          leds[i].setRGB(255, 200, 0);
+          break;
+      }
+    } else {
+      leds[i].setRGB(0, 0, 0);
     }
   }
   FastLED.show();
+}
+
+void btnTick() {
+  btn.tick();
+  if (btn.hasClicks(1)) db[kk::state] = !db[kk::state];
+  else if (btn.hasClicks(2)) db[kk::brightness] += (byte)db[kk::plusBrightness];
+  else if (btn.hasClicks(3)) db[kk::brightness] -= (byte)db[kk::plusBrightness];
+  else if (btn.hold(1)) ESP.restart();
+  else if (btn.hold(2)) gData.scene = ++gData.scene % 4;
+  else if (btn.hold(3)) gData.color = ++gData.color % 8;
+  else return;
+  light();
 }
