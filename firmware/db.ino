40217e7d22a042fdd41d7ef1901c85b4ff449696
MagmaXY <149756117+MagmaXY@users.noreply.github.com>
2025-04-25 15:22:29 +0700

    Update db.ino

--- firmware/db.ino
+++ firmware/db.ino
@@ -1,91 +1,148 @@
-void gestTick() {
+void db_init() {
+  LittleFS.begin();
+  db.begin();
 
-  if (g.gest == "") return;
+  db.init(kk::ssid, "53");
+  db.init(kk::pass, "Zz11116666");
+  db.init(kk::ssid1, "Dima");
+  db.init(kk::pass1, "MagmaXY2020");
+  db.init(kk::host, "test.mosquitto.org");
+  db.init(kk::port, 1883);
+  db.init(kk::header, "GXY:");
+  db.init(kk::names, "GXY_Контроллер,GXY_Медиа,GXY_Лампа");
+  db.init(kk::scenes, "Нет,Утро,Уход,Приход,Кино,Сон");
+  db.init(kk::sep, ',');
+  db.init(kk::serial, 115200);
+  db.init(kk::searchs, 2000);
+  db.init(kk::ons, 5000);
+  db.init(kk::brightness, 127);
+  db.init(kk::turn, 180);
+  db.init(kk::power, 255);
+  db.init(kk::state, false);
+  db.init(kk::reaction, 400);
+  db.init(kk::quit, 800);
+  db.init(kk::connect, false);
+  db.init(kk::stateGest, 2);
+  db.init(kk::vibrationGest, 3);
+  db.init(kk::plusBrightnessGest, 0);
+  db.init(kk::plusBrightness, 32);
+  db.init(kk::minusBrightnessGest, 1);
+  db.init(kk::nextNameGest, 6);
+  db.init(kk::previousNameGest, 7);
+  db.init(kk::nextSceneGest, 4);
+  db.init(kk::previousSceneGest, 5);
+  db.init(kk::sceneGest, 0);
+}
 
-  if (!db[kk::state] and g.gest == g.getGest(db[kk::stateGest])) {
-
-    db[kk::state] = true;
-    Serial.println("Активный");
-    vibro.on(500);
-    cursorGraph();
-    gData.timer = millis();
-    return;
-
-  } else if (db[kk::state] and gData.name == 0 and gData.scene == 0 and g.gest == g.getGest(db[kk::stateGest])) {
-    db[kk::state] = false;
-    Serial.println("Неактивный");
-    oled.clear();
-    gData.timer = millis();
-    return;
+void build(sets::Builder& b) {
+  {
+    sets::Group g(b, "🤖 Датчик");
+    b.Slider(kk::reaction, "⏲ Реакция", 100, 1000, 100);
+    b.Slider(kk::quit, "⏲ Ожидание", 100, 1000, 100);
+    b.Slider(kk::turn, "🔄 Поворот", 0, 270, 90);
+  }
+  {
+    sets::Group g(b, "🖼️ Дисплей");
+    b.Slider(kk::brightness, "🔆 Яркость", 0, 255, 1);
+    b.Slider(kk::ons, "🔅 Активное управление", 0, 15000, 1000);
+    b.Switch(kk::state, "📴 Управление");
+    if (b.Button("fill"_h, "🪣 Залить")) oled.rect(0, 0, 128, 31, OLED_FILL);
+  }
+  {
+    sets::Group g(b, "📳 Вибрация");
+    b.Slider(kk::power, "📲 Сила", 0, 255, 1);
+    b.Slider(kk::searchs, "🕑 Поиск", 0, 60000, 1);
+    if (b.Button("search"_h, "🔎 Поиск")) {
+      vibro.on(db[kk::searchs]);
+    }
+  }
+  {
+    sets::Group g(b, "👋 Жесты");
+    String gests = "Вперёд;Назад;Вперёд-Назад;Назад-Вперёд;Вправо;Влево;Вправо-Влево;Влево-Вправо;Вниз;Вверх;Вниз-Вверх;Вверх-Вниз;По часовой;Против часовой;Волна";
+    b.Select(kk::stateGest, "📴 Управление", gests);
+    b.Select(kk::vibrationGest, "🚩 Вибрация", gests);
+    b.Select(kk::plusBrightnessGest, "🔆 Яркость", gests);
+    b.Slider(kk::plusBrightness, "🎇 Изменение яркости", 0, 255, 1);
+    b.Select(kk::minusBrightnessGest, "🔅 Яркость", gests);
+    b.Select(kk::nextNameGest, "🔜 Устройство", gests);
+    b.Select(kk::previousNameGest, "🔚 Устройство", gests);
+    b.Select(kk::nextSceneGest, "🔜 Сценарий", gests);
+    b.Select(kk::previousSceneGest, "🔚 Сценарий", gests);
+    b.Select(kk::sceneGest, "📨 Сценарий", gests);
+  }
+  {
+    sets::Group g(b, "👨‍💻 Serial");
+    b.Number(kk::serial, "🕓 Бод");
+  }
+  {
+    sets::Group g(b, "⚙ MQTT");
+    b.Input(kk::host, "🖥 Хост");
+    b.Number(kk::port, "🔢 Порт");
+    b.LED(kk::connect, "☁ Статус", mqtt.connected());
+    b.Input(kk::header, "📨 Заголовок");
+    b.Input(kk::sep, "✏️ Символ-разделитель");
+    b.Input(kk::names, "📜 Устройства");
+    b.Input(kk::scenes, "📑 Сценарии");
+  }
+  {
+    sets::Group g(b, "📶 WiFi");
+    b.Input(kk::ssid, "🌐 Сеть");
+    b.Pass(kk::pass, "🔑 Пароль");
+    b.Input(kk::ssid1, "🌐 реСеть");
+    b.Pass(kk::pass1, "🔑 реПароль");
+  }
+  {
+    if (b.Button("zavod"_h, "💾 Сброс до заводских настроек")) {
+      db.clear();
+      ESP.restart();
+    }
+    if (b.Button("setup"_h, "🔂 Перезагрузить")) {
+      ESP.restart();
+    }
   }
 
-  if (!db[kk::state]) return;
+  {
+    b.Link("🔗 GestureXY от MagmaXY", "https://github.com/MagmaXY/GestureXY");
+  }
 
-  if (gData.scene == 0) {
 
-    if (g.gest == g.getGest(db[kk::nextNameGest]) or g.gest == g.getGest(db[kk::previousNameGest])) {
-
-      int8_t move = g.gest == g.getGest(db[kk::nextNameGest]) ? 1 : -1;
-      if (gData.name + move < 0) {
-        gData.name = getNamesCount() - 1;
-      } else {
-        gData.name = (gData.name + move) % getNamesCount();
-      }
-      cursorGraph();
-
-    } else if (gData.name == 0) {
-
-      if (g.gest == g.getGest(db[kk::nextSceneGest]) or g.gest == g.getGest(db[kk::previousSceneGest])) {
-
-        int8_t move = g.gest == g.getGest(db[kk::nextSceneGest]) ? 1 : -1;
-        if (gData.scene + move < 0) {
-          gData.scene = getScenesCount() - 1;
-        } else {
-          gData.scene = (gData.scene + move) % getScenesCount();
+  if (b.build.isAction()) {
+    switch (b.build.id) {
+      case kk::brightness:
+        oled.setContrast(b.build.value);
+        break;
+      case kk::state:
+        Serial.println(b.build.value.toInt() ? "Активный" : "Неактивный");
+        oled.clear();
+        if (b.build.value.toInt()) {
+          vibro.on(500);
+          cursorGraph();
         }
-        Serial.println(getScene());
-        cursorGraph();
-
-      } else if (g.gest == g.getGest(db[kk::plusBrightnessGest])) {
-
-        db[kk::brightness] = ((byte)db[kk::brightness] + (byte)db[kk::plusBrightness]) % 256;
-        cursorGraph();
-        oled.setContrast(db[kk::brightness]);
-
-      } else if (g.gest == g.getGest(db[kk::minusBrightnessGest])) {
-
-        db[kk::brightness] -= (byte)db[kk::plusBrightness];
-        if (db[kk::brightness] < 0) db[kk::brightness] = 255;
-        cursorGraph();
-        oled.setContrast(db[kk::brightness]);
-
-      } else if (g.gest == g.getGest(db[kk::vibrationGest])) {
-
-        db[kk::power] = !db[kk::power] * 255;
-        cursorGraph();
-      }
-    } else {
-
-      cursorGraph();
-      oled.print(g.getRUGest());
-      sendGest();
+        break;
+      case kk::names:
+        if (db[kk::state] and gData.scene == 0) cursorGraph();
+        break;
+      case kk::scenes:
+        if (!b.build.value.startsWith("Нет,"))
+          db[kk::scenes] = "Нет," + String(b.build.value);
+        if (db[kk::state] and gData.scene != 0) cursorGraph();
+        break;
+      case kk::turn:
+        g.setTurn(b.build.value);
+        break;
+      case kk::sep:
+        if (db[kk::state]) cursorGraph();
+        break;
+      case kk::quit:
+        g.setQuit(b.build.value);
+        break;
+      case kk::reaction:
+        g.setReaction(b.build.value);
+        break;
+      case kk::power:
+        vibro.setPower(b.build.value);
+        break;
     }
-  } else if (g.gest == g.getGest(db[kk::nextSceneGest]) or g.gest == g.getGest(db[kk::previousSceneGest])) {
-
-    int8_t move = g.gest == g.getGest(db[kk::nextSceneGest]) ? 1 : -1;
-    if (gData.scene + move < 0) {
-      gData.scene = getScenesCount() - 1;
-    } else {
-      gData.scene = (gData.scene + move) % getScenesCount();
-    }
-    Serial.println(getScene());
-    cursorGraph();
-
-  } else if (g.gest == g.getGest(db[kk::sceneGest])) {
-
-    Serial.println(getScene());
-    cursorGraph();
-    sendScene();
+    gData.timer = millis();
   }
-  gData.timer = millis();
 }
